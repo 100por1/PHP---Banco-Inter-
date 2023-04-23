cf51008661caaa461d6ec9ea5bebee994975c374
100por1 <wilton@100por1.com>
2023-04-23 12:02:27 -0300

    Create config-inter-pix.php

---
+++ config-inter-pix.php
@@ -0,0 +1,16 @@
+<?php
+#Chave PIX qualquer criada na conta, uso sempre uma generica
+$chave_pix='------';
+#Conta Banco Inter
+$conta_corrente='------';
+
+#Cliente ID criado na API Inter
+$client_id='-----';
+#Client Secret Criado na API Inter
+$client_secret='-----';
+
+#Local onde salvou o arquivo CRT criado com a API Inter
+$clientCert = "/inc/Inter-API_Certificado.crt";
+#Local onde salvou o arquivo KEY criado com a API Inter
+$clientKey = "/inc/Inter-API_Chave.key";
+?>
